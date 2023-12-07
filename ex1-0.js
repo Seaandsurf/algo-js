c29c0f777aa978c8eb2e0fbb5e4012d26e57efbc
Seaandsurf <mathias.hardat@gmail.com>
2023-12-07 14:57:37 +0100

    addition

---
+++ ex1-0.js
@@ -0,0 +1,20 @@
+let monage = 32;
+console.log(monage);
+
+let friendage = 28;
+console.log(friendage);
+
+denisage = monage - 7;
+console.log(denisage);
+
+noeage = friendage / 3;
+console.log(noeage);
+
+let ugurage = (hussyeinage % 9) * denisage;
+console.log(ugurage);
+
+let robinage = hussyeinage + denisage / 8 + 2;
+console.log(robinage)
+
+
+
