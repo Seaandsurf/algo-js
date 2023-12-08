cdb60525cee9fd9aa5005dc0faa2198d1c1ac21c
Seaandsurf <mathias.hardat@gmail.com>
2023-12-08 15:56:34 +0100

    avancement sur algo JS

---
+++ exo-1/ex1-1.js
@@ -0,0 +1,13 @@
+let exo = "Exercice 1.1"
+console.log(exo)
+
+const monNom = "Mathias";
+console.log(monNom);
+
+let monAge = 32;
+console.log(monAge)
+
+console.log("Je m'appelle " + monNom + " et j'ai " + monAge + " ans")
+
+
+
